ea5ea022ff5886a01e598105f9b6b383fa2c632f
François Risoud <francois.risoud@gmail.com>
2022-04-05 23:05:58 +0200

    feat: add map component

---
+++ src/components/FMap.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="map" style="height: 400px"></div>
+</template>
+
+<script setup lang="ts">
+import { Map, LngLatLike } from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { onMounted } from "vue";
+
+export interface FMapProps {
+  /**
+   * The map initial center coordinates
+   */
+  center: LngLatLike;
+}
+const props = defineProps<FMapProps>();
+
+onMounted(() => {
+  new Map({
+    accessToken: import.meta.env.VITE_MAPBOX_ACCESS_TOKEN,
+    container: "map",
+    style: "mapbox://styles/mapbox/light-v10",
+    center: props.center,
+    zoom: 9,
+  });
+});
+</script>
