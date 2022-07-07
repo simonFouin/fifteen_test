1fb657b2b093bcaec26b73a3c1694b72760bae35
Simon F <sfouin@aquiladata.fr>
2022-07-07 12:02:44 +0200

    update marker icons and colors management

--- src/components/MapPopup.vue
+++ src/components/MapPopup.vue
@@ -1,11 +1,11 @@
 <template>
   <v-card>
     <template v-slot:title>
-      <div class="popup-head popup-flex" :class="headColor">
+      <div class="popup-head popup-flex" :class="stateInfos.color">
         <v-icon
           :title="bike.in_order ? ServiceStatus[bike.service_status] : 'maintenance'"
           class="popup-state">
-          {{ bike.in_order ? stateIcon : "mdi-alert" }}
+          {{ stateInfos.icon }}
         </v-icon>
         <h3 class="popup-title">Bike {{ bike.serial_number }}</h3>
       </div>
@@ -15,7 +15,7 @@
       <div
         :title="`${bike.battery_level}%`"
         class="popup-battery popup-flex">
-        <v-icon>mdi-battery-{{ Math.round(bike.battery_level / 10) * 10 }}</v-icon>
+        <v-icon>{{ batteryIcon }}</v-icon>
         <v-progress-linear
           :model-value="bike.battery_level"
           background-color="pink lighten-3"
@@ -33,44 +33,49 @@ import { computed } from 'vue';
 import Bike, { ServiceStatus } from '@/interface/Bike';
 
 interface MapPopupProps {
-  //  The map initial center coordinates
   bike: Bike;
 }
 const { bike } = defineProps<MapPopupProps>();
 
-const headColor = computed(() => {
+const stateInfos = computed<{ color: string, icon: string }>(() => {
   if (!bike.in_order) {
-    return 'red';
+    return {
+      color: 'red',
+      icon: 'mdi-alert',
+    };
   }
   switch (bike.service_status) {
     case 1:
-      return 'green';
+      return {
+      color: 'green',
+      icon: 'mdi-comment-check',
+    };
     case 2:
-      return 'orange';
+      return {
+      color: 'orange',
+      icon: 'mdi-lock',
+    };
     default:
-      return 'red';
+      return {
+      color: 'red',
+      icon: 'mdi-bicycle',
+    };
   };
 });
 
-const stateIcon = computed(() => {
-  switch (bike.service_status) {
-    case 1:
-      return 'mdi-comment-check';
-    case 2:
-      return 'mdi-lock';
-    default:
-      return 'mdi-bicycle';
-  };
-})
-
 const batteryColor = computed(() => {
   if (bike.battery_level > 70) {
-    return "green";
+    return 'green';
   }
-  if (bike.battery_level > 35) {
-    return "orange";
+  return bike.battery_level > 35 ? 'orange' : 'red';
+});
+
+const batteryIcon = computed(() => {
+  const roundedBattery = Math.round( bike.battery_level / 10) * 10;
+  if (roundedBattery === 0) {
+    return 'mdi-battery-outline';
   }
-  return "red";
+  return roundedBattery >= 100 ? 'mdi-battery' : `mdi-battery-${roundedBattery}`;
 });
 </script>
 
@@ -97,7 +102,7 @@ const batteryColor = computed(() => {
     position: absolute;
     content: "\00d7";
     font-size: 2.5rem;
-    opacity: 0.6;
+    opacity: 0.7;
     z-index: 1;
   }
 
